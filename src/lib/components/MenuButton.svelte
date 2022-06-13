4e6ff190fe2309a9413485a3463b7aa3de77096b
jadnw <jadenwu137@protonmail.com>
2022-06-13 21:30:31 +0700

    :recycle: Use MenuButton as a separated component

---
+++ src/lib/components/MenuButton.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  let open = false
+
+  const toggle = () => (open = !open)
+</script>
+
+<button class="menu-button" class:open on:click={toggle}>
+  <div>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+    <span>&nbsp;</span>
+  </div>
+</button>
+
+<style lang="scss">
+  .menu-button {
+    cursor: pointer;
+    width: 2.25rem;
+    height: 2.25rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    div {
+      position: relative;
+      width: 1.25rem;
+      height: 0.75rem;
+    }
+
+    span {
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      display: block;
+      background-color: var(--theme-fg);
+      transition: all 0.15s ease-out;
+    }
+
+    span:nth-child(1) {
+      top: 0;
+    }
+
+    &.open span:nth-child(1) {
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+
+    &.open span:nth-child(2) {
+      transform: translateX(-1rem);
+      opacity: 0;
+    }
+
+    span:nth-child(2) {
+      top: 5px;
+    }
+
+    span:nth-child(3) {
+      top: 10px;
+    }
+
+    &.open span:nth-child(3) {
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+  }
+</style>
