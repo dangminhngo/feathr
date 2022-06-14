3b82c720f78b79d5d2f4371ce7a13a4e4a3c81d4
jadnw <jadenwu137@protonmail.com>
2022-06-14 09:17:17 +0700

    :sparkles: Add ProfilePictureButton component

--- src/layouts/Header.svelte
+++ src/layouts/Header.svelte
@@ -4,12 +4,16 @@
   import MenuButton from '$lib/components/MenuButton.svelte'
   import SearchBox from '$lib/components/SearchBox.svelte'
   import IconButton from '$lib/components/IconButton.svelte'
+  import ProfilePictureButton from '$lib/components/ProfilePictureButton.svelte'
 </script>
 
 <header class="header">
   <MenuButton on:click={uiStore.toggleNavShrink} />
   <SearchBox placeholder="Search notes or tasks ..." />
-  <IconButton name="cog" on:click={() => console.log('Clicked')} />
+  <div class="buttons">
+    <IconButton name="cog" on:click={() => console.log('Clicked')} />
+    <ProfilePictureButton />
+  </div>
 </header>
 
 <style lang="scss">
@@ -19,5 +23,12 @@
     align-items: center;
     gap: 1.5rem;
     border-bottom: 1px solid var(--theme-primary-700);
+  }
+
+  .buttons {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
   }
 </style>
---
+++ src/lib/components/ProfilePictureButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let url =
+      'https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png',
+    email = 'jacktheripper@gmail.com'
+</script>
+
+<button>
+  <img src={url} alt={email} />
+</button>
+
+<style lang="scss">
+  button {
+    cursor: pointer;
+    width: var(--button-height);
+    height: var(--button-height);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: var(--rounded);
+    transition: background-color 0.15s ease-out;
+  }
+
+  button:hover {
+    background-color: var(--theme-primary-700);
+  }
+
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 100%;
+  }
+</style>
