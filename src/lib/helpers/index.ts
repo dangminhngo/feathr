b0f305fbec3693ff627ddbb5915e35344b04d7f8
jadnw <jadenwu137@protonmail.com>
2022-06-15 12:13:20 +0700

    :hammer: Add a function to check if a note is empty

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -12,6 +12,22 @@ export const createEmptyNote = (): Note => ({
   trash: false,
 })
 
+export const checkEmptyNote = (note: Note): boolean => {
+  const emptyNote = createEmptyNote()
+
+  if (
+    note.id === emptyNote.id &&
+    note.title === '' &&
+    note.body === '' &&
+    !note.images.length &&
+    !note.tagIds.length
+  ) {
+    return true
+  }
+
+  return false
+}
+
 export const clickOutside: Action<HTMLElement, undefined> = (node: HTMLElement) => {
   const handleClick = (e: MouseEvent) => {
     if (node && !node.contains(e.target as HTMLElement)) {
