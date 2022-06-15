a90ade2cb4e7c69c32105b887879ba525b428066
jadnw <jadenwu137@protonmail.com>
2022-06-15 20:56:26 +0700

    :sparkles: Add Modal component

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Portal from 'svelte-portal/src/Portal.svelte'
+</script>
+
+<Portal target="body">
+  <div class="backdrop">&nbsp;</div>
+  <slot />
+</Portal>
+
+<style lang="scss">
+  .backdrop {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: hsla(0, 0%, 0%, 0.2);
+  }
+</style>
