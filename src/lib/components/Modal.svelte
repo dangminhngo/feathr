7eef7a6fc62fb3a8a5e9a350645a556a383022e1
jadnw <jadenwu137@protonmail.com>
2022-06-16 11:05:08 +0700

    :hammer: Add NoteForm modal

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Portal from 'svelte-portal/src/Portal.svelte'
+  export let width = 30
 </script>
 
 <Portal target="body">
   <div class="backdrop">&nbsp;</div>
-  <slot />
+  <div class="wrapper" style="width: {width}rem;"><slot /></div>
 </Portal>
 
 <style lang="scss">
@@ -14,6 +15,14 @@
     left: 0;
     width: 100vw;
     height: 100vh;
-    background-color: hsla(0, 0%, 0%, 0.2);
+    background-color: hsla(0, 0%, 0%, 0.6);
+  }
+
+  .wrapper {
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    transform: translate(-50%, 0);
+    background-color: var(--theme-primary-900);
   }
 </style>
--- src/routes/app/notes.svelte
+++ src/routes/app/notes.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import FormField from '$lib/components/FormField.svelte'
   import NoteGrid from '$containers/NoteGrid.svelte'
+  import NoteForm from '$containers/NoteForm.svelte'
+  import Modal from '$lib/components/Modal.svelte'
 
   import { notesStore } from '$lib/stores'
 
@@ -9,3 +11,6 @@
 
 <FormField />
 <NoteGrid notes={notes.filter((n) => !n.trash)} />
+<Modal>
+  <NoteForm />
+</Modal>
